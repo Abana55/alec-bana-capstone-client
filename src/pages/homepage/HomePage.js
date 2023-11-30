eb88baea13313db22aa2c49fdcc97df81c16637d
Alec bana <anbana994@gmail.com>
2023-11-30 03:15:21 -0500

    added styles for footer

--- src/pages/homepage/HomePage.js
+++ src/pages/homepage/HomePage.js
@@ -1,18 +1,26 @@
 import React from 'react';
 import './HomePage.scss';
+import hero from '../../assets/hero-img.jpg'
 
 function HomePage() {
   return (
     <>
+      
       <section className="home">
         <div className="home__hero">
-          <h1 className="home__title">Empower Your Financial Journey with Summa</h1>
-          <p className="home__subtitle">Dynamic charts and graphs to simplify your financial planning.</p>
-          <button className="home__cta">Get Started</button>
+          <img
+            className="home__hero-img"
+            src={hero}
+            alt="Financial Journey"
+          />
+          <div className="home__hero-text">
+            <h1 className="home__title">Empower Your Financial Journey with Summa</h1>
+            <p className="home__subtitle">Dynamic charts and graphs to simplify your financial planning.</p>
+          </div>
         </div>
 
         <p className="home__mission">
-          Summa is a new way for people to prepare for financial endeavors by using dynamic charts and graphs to calculate loans.
+          Summa is a new way for people to prepare for financial endeavors by using dynamic charts and graphs to calculate financial goals.
         </p>
 
         <div className="home__features">
