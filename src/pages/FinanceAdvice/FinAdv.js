032591e88b3fdd2d51d70786a66db86139f7d47b
Alec Bana <anbana994@gmail.com>
2024-03-11 16:53:48 -0400

    added new page for investing

---
+++ src/pages/FinanceAdvice/FinAdv.js
@@ -0,0 +1,16 @@
+import './FinAdv.scss';
+
+function FinancialAdvisor() {
+    return (
+        <>
+        <section>
+    <h2>Stock Insights</h2>
+    {/* Components for displaying stock market information */}
+        </section>
+
+        
+        </>
+    )
+}
+
+export default FinancialAdvisor;
